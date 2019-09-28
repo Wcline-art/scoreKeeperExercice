28ffab33c7e65b0bf46cc322fd601d272b20f805
Celine Worm <wormceline@gmail.com>
2019-09-28 19:26:14 +0200

    declare all constants variables for cibling scores and buttons elements in script file

---
+++ script.js
@@ -0,0 +1,7 @@
+const scorePlayer1 = document.querySelectorAll("span")[0];
+const scorePlayer2 = document.querySelectorAll("span")[1];
+const winnerScoreMax = document.querySelectorAll("span")[2];
+const input = document.querySelector("input");
+const buttonPlayer1 = document.querySelectorAll("button")[0];
+const buttonPlayer2 = document.querySelectorAll("button")[1];
+const reset = document.querySelectorAll("button")[2];
