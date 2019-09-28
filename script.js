4bb5f2fc0f4f3d5a087ffb4cf9d12425e0e55fd9
Celine Worm <wormceline@gmail.com>
2019-09-28 23:22:04 +0200

    add player and scoremax functions in script file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// CONS variables
+// CONS variables for cibling
 const scorePlayer1 = document.querySelectorAll("span")[0];
 const scorePlayer2 = document.querySelectorAll("span")[1];
 const winnerScoreMax = document.querySelectorAll("span")[2];
@@ -12,3 +12,24 @@ let inputScoreMax = input.value;
 let scorePl1 = 0;
 let scorePl2 = 0;
 let scoreMax = 5;
+
+function boostScorePlayer1() {
+  scorePlayer1.textContent = scorePl1;
+  scorePl1++;
+}
+
+function boostScorePlayer2() {
+  scorePlayer2.textContent = scorePl2;
+  scorePl2++;
+}
+
+function addScoreMaxInput() {
+  input.innerText = input.value;
+}
+
+function addScoreInWinnerScoreMax() {
+  winnerScoreMax.textContent = scoreMax;
+}
+
+buttonPlayer1.addEventListener("click", boostScorePlayer1);
+buttonPlayer2.addEventListener("click", boostScorePlayer2);
