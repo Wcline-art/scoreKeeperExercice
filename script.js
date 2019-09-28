c381fa0e809c09bc9da3f3039673872bc9e39be9
Celine Worm <wormceline@gmail.com>
2019-09-28 19:44:06 +0200

    createvariables for elements values in script file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CONS variables
 const scorePlayer1 = document.querySelectorAll("span")[0];
 const scorePlayer2 = document.querySelectorAll("span")[1];
 const winnerScoreMax = document.querySelectorAll("span")[2];
@@ -5,3 +6,9 @@ const input = document.querySelector("input");
 const buttonPlayer1 = document.querySelectorAll("button")[0];
 const buttonPlayer2 = document.querySelectorAll("button")[1];
 const reset = document.querySelectorAll("button")[2];
+
+// lET variables
+let inputScoreMax = input.value;
+let scorePl1 = 0;
+let scorePl2 = 0;
+let scoreMax = 5;
